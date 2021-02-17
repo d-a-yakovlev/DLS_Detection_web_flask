f2302b88fc120c06ae8cf59bc89b43c6e90cf505
SmellsLikeStudentSpirit <raidegdp@gmail.com>
2021-02-17 15:23:08 +0300

    sixth

--- app.py
+++ app.py
@@ -14,14 +14,13 @@ import sys
 
 flags.FLAGS(sys.argv)
 
-# customize your API through the following parameters
+
 classes_path = './data/labels/coco.names'
 weights_path = './weights/yolov3-tiny.tf'
-tiny = True                    # set to True if using a Yolov3 Tiny model
-size = 416                      # size images are resized to for model
-output_path = './static/detections/'   # path to output folder where images with detections are saved
-num_classes = 80                # number of classes in model
-
+tiny = True                    
+size = 416                      
+output_path = './static/detections/'   
+num_classes = 80                
 upload_path='./static/uploads/'
 
 
@@ -54,66 +53,7 @@ def index():
 def about():
   return render_template("about.html")
 
-'''
-# API that returns JSON with classes found in images
-@app.route('/detections', methods=['POST'])
-def get_detections():
-    raw_images = []
-    images = request.files.getlist("images")
-    image_names = []
-    for image in images:
-        image_name = image.filename
-        image_names.append(image_name)
-        image.save(os.path.join(os.getcwd(), image_name))
-        img_raw = tf.image.decode_image(
-            open(image_name, 'rb').read(), channels=3)
-        raw_images.append(img_raw)
-        
-    num = 0
-    
-    # create list for final response
-    response = []
 
-    for j in range(len(raw_images)):
-        # create list of responses for current image
-        responses = []
-        raw_img = raw_images[j]
-        num+=1
-        img = tf.expand_dims(raw_img, 0)
-        img = transform_images(img, size)
-
-        t1 = time.time()
-        boxes, scores, classes, nums = yolo(img)
-        t2 = time.time()
-        print('time: {}'.format(t2 - t1))
-
-        print('detections:')
-        for i in range(nums[0]):
-            print('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
-                                            np.array(scores[0][i]),
-                                            np.array(boxes[0][i])))
-            responses.append({
-                "class": class_names[int(classes[0][i])],
-                "confidence": float("{0:.2f}".format(np.array(scores[0][i])*100))
-            })
-        response.append({
-            "image": image_names[j],
-            "detections": responses
-        })
-        img = cv2.cvtColor(raw_img.numpy(), cv2.COLOR_RGB2BGR)
-        img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
-        cv2.imwrite(output_path + 'detection' + str(num) + '.jpg', img)
-        print('output saved to: {}'.format(output_path + 'detection' + str(num) + '.jpg'))
-
-    #remove temporary images
-    for name in image_names:
-        os.remove(name)
-    try:
-        return jsonify({"response":response}), 200
-    except FileNotFoundError:
-        abort(404)
-'''
-# API that returns image with detections on it
 @app.route('/uploader', methods= ['GET','POST'])
 def get_image():
     image = request.files['file']
